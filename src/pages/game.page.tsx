895bbe6165fc596d4e3758f2a60119a4211915f7
moshew <Moshewiner13@gmail.com>
2020-05-27 03:42:07 +0300

    Added the ability to change board size and mines count

--- src/pages/game.page.tsx
+++ src/pages/game.page.tsx
@@ -1,9 +1,21 @@
 import Board from "../components/board/board.component";
-import React from "react";
+import React, { useState, useContext } from "react";
 import './game.page.scss'
 
+
 function Game() {
-    return <Board colsCount={4} rowsCount={7} mineCount={7} />
+
+    let [colsCount, setColsCount] = useState(6);
+    let [rowsCount, setRowsCount] = useState(6);
+    let [minesCount, setMinesCount] = useState(3);
+
+    return (
+        <>
+            <input type='number' placeholder="Number of columns" onChange={(e) => setColsCount(+e.target.value)} />
+            <input type='number' placeholder="Number of rows" onChange={(e) => setRowsCount(+e.target.value)} />
+            <input type='number' placeholder="Number of mines" onChange={(e) => setMinesCount(+e.target.value)} />
+            <Board colsCount={colsCount} rowsCount={rowsCount} mineCount={minesCount} />
+        </>);
 }
 
 export default Game;
