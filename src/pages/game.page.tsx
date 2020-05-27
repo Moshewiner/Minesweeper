052d3e7dc85d5339102c7a21e7a43152cc664da1
moshew <Moshewiner13@gmail.com>
2020-05-28 01:27:44 +0300

    game aligned to center

--- src/pages/game.page.tsx
+++ src/pages/game.page.tsx
@@ -17,8 +17,8 @@ export const GameContext = React.createContext<{
 });
 
 function Game() {
-  const [colsCount, setColsCount] = useState(6);
-  const [rowsCount, setRowsCount] = useState(6);
+  const [colsCount, setColsCount] = useState(9);
+  const [rowsCount, setRowsCount] = useState(9);
   const [minesCount, setMinesCount] = useState(3);
   const [boardProps, setBoardProps] = useState({
     colsCount,
@@ -30,51 +30,62 @@ function Game() {
   const [boardId, setBoardId] = useState(0);
 
   return (
-    <GameContext.Provider
-      value={{ remainingFlagsCount, isGameRunning, setFlagCount, minesCount, setIsGameRunning }}
-    >
-      <input
-        value={colsCount}
-        type="number"
-        placeholder="Number of columns"
-        onChange={(e) => setColsCount(+e.target.value)}
-      />
-      <input
-        value={rowsCount}
-        type="number"
-        placeholder="Number of rows"
-        onChange={(e) => setRowsCount(+e.target.value)}
-      />
-      <input
-        value={minesCount}
-        type="number"
-        placeholder="Number of mines"
-        onChange={(e) => {
-          setMinesCount(+e.target.value);
-          setFlagCount(+e.target.value);
-        }}
-      />
-      <button
-        onClick={
-          () => {
-            setBoardProps({ colsCount, rowsCount, minesCount });
-            setIsGameRunning(true);
-            setBoardId(boardId+1);
-          }
-        }
-      >
-        New Game
-      </button>
-      <div>
-        <span>Remaining Flags: {remainingFlagsCount}</span>
+    <div className='game'>
+      <div className='heading'>
+        Minesweeper
       </div>
-      <Board
-        colsCount={boardProps.colsCount}
-        rowsCount={boardProps.rowsCount}
-        mineCount={boardProps.minesCount}
-        id={boardId}
-      />
-    </GameContext.Provider>
+      <GameContext.Provider
+        value={
+          {
+            remainingFlagsCount,
+            isGameRunning,
+            setFlagCount,
+            minesCount,
+            setIsGameRunning
+          }
+        }>
+        <div className='controls'>
+          <input
+            value={colsCount}
+            type="number"
+            placeholder="Number of columns"
+            onChange={(e) => setColsCount(+e.target.value)}
+          />
+          <input
+            value={rowsCount}
+            type="number"
+            placeholder="Number of rows"
+            onChange={(e) => setRowsCount(+e.target.value)}
+          />
+          <input
+            value={minesCount}
+            type="number"
+            placeholder="Number of mines"
+            onChange={(e) => {
+              setMinesCount(+e.target.value);
+              setFlagCount(+e.target.value);
+            }}
+          />
+          <button onClick={
+            () => {
+              setBoardProps({ colsCount, rowsCount, minesCount });
+              setIsGameRunning(true);
+              setBoardId(boardId + 1);
+            }
+          }>New Game</button>
+
+        </div>
+        <div className="info">
+          <span>Remaining Flags: {remainingFlagsCount}</span>
+        </div>
+        <Board
+          colsCount={boardProps.colsCount}
+          rowsCount={boardProps.rowsCount}
+          mineCount={boardProps.minesCount}
+          id={boardId}
+        />
+      </GameContext.Provider>
+    </div>
   );
 }
 
