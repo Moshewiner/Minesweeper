2495262dedd25303d31464b054ade7882b0bf235
moshew <Moshewiner13@gmail.com>
2020-05-27 20:40:30 +0300

    Added context to handle flags counts

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -1,168 +1,180 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import './board.component.scss';
 import TileContainer from '../tiles/tile.container';
 import { twoDimentionalIndexToOne } from '../../services/array-utils/array-utils.service';
 import {
-  createBoard,
-  findNeighbours,
+	createBoard,
+	findNeighbours,
 } from '../../services/board/board.service';
 import { TileStatus, ClickType } from '../tiles/tile.component';
 import {
-  DEFAULT_MINE_TILE,
-  Tile,
-  TileType,
+	DEFAULT_MINE_TILE,
+	Tile,
+	TileType,
 } from '../../services/board/board.types';
 import { FixedSizeGrid as Grid } from 'react-window';
+import { GameContext } from './../../pages/game.page';
+
 
 function Board(props: {
-  colsCount: number;
-  rowsCount: number;
-  mineCount: number;
+	colsCount: number;
+	rowsCount: number;
+	mineCount: number;
 }) {
-  let [board, setBoard] = useState<Tile[]>([]);
 
-  const onMineClick = useCallback(
-    (clickType: ClickType, tileStatus: TileStatus, position: number) => {
-      if (board.length !== props.colsCount * props.rowsCount) {
-      } else if (clickType === ClickType.Primary) {
-        board.length &&
-          board.forEach((tile: Tile) => {
-            if (tile.type === TileType.Mine) {
-              tile.status = TileStatus.Revealed;
-            }
-          });
-        setTimeout(() => alert('You have lost!'), 0);
-      } else if (clickType === ClickType.Secondary) {
-        board[position].status = toggleFlag(tileStatus);
-      }
-      setBoard([...board]);
-    },
-    [board]
-  );
+	const { isGameRunning, flagCount, setFlagCount } = useContext(GameContext);
+	let [board, setBoard] = useState<Tile[]>([]);
 
-  const onNumberTileClick = useCallback(
-    (clickType: ClickType, tileStatus: TileStatus, position: number) => {
-      if (board.length !== props.colsCount * props.rowsCount) {
-      } else if (clickType === ClickType.Primary) {
-        const n = revealTile(tileStatus);
-        board[position].status = n;
+	const onMineClick = useCallback(
+		(clickType: ClickType, tileStatus: TileStatus, position: number) => {
+			if (board.length !== props.colsCount * props.rowsCount) {
+			} else if (clickType === ClickType.Primary) {
+				board.length &&
+					board.forEach((tile: Tile) => {
+						if (tile.type === TileType.Mine) {
+							tile.status = TileStatus.Revealed;
+						}
+					});
+				setTimeout(() => alert('You have lost!'), 0);
+			} else if (clickType === ClickType.Secondary) {
+				board[position].status = toggleFlag(tileStatus, flagCount, setFlagCount);
+			}
+			setBoard([...board]);
+		},
+		[board]
+	);
 
-        if (board[position].value === 0) {
-          revealEmptyTiles(board, position, props.colsCount, props.rowsCount);
-        }
-      } else if (clickType === ClickType.Secondary) {
-        board[position].status = toggleFlag(board[position].status);
-      }
-      setBoard([...board]);
-    },
-    [board, props.colsCount, props.rowsCount]
-  );
+	const onNumberTileClick = useCallback(
+		(clickType: ClickType, tileStatus: TileStatus, position: number) => {
+			if (board.length !== props.colsCount * props.rowsCount) {
+			} else if (clickType === ClickType.Primary) {
+				const n = revealTile(tileStatus);
+				board[position].status = n;
 
-  useEffect(() => {
-    setBoard(createBoard(props.colsCount, props.rowsCount, props.mineCount));
-  }, [props.colsCount, props.rowsCount, props.mineCount]);
+				if (board[position].value === 0) {
+					revealEmptyTiles(board, position, props.colsCount, props.rowsCount);
+				}
+			} else if (clickType === ClickType.Secondary) {
+				board[position].status = toggleFlag(board[position].status, flagCount, setFlagCount);
+			}
+			setBoard([...board]);
+		},
+		[board, props.colsCount, props.rowsCount]
+	);
 
-  return board.length > 0 ? (
-    <Grid
-      columnCount={props.colsCount}
-      rowCount={props.rowsCount}
-      columnWidth={50}
-      width={500}
-      height={500}
-      rowHeight={50}
-    >
-      {({ columnIndex, rowIndex, style }) => {
-        return createCell(
-          style,
-          twoDimentionalIndexToOne(columnIndex, rowIndex, props.colsCount),
-          board,
-          onMineClick,
-          onNumberTileClick
-        );
-      }}
-    </Grid>
-  ) : null;
+	useEffect(() => {
+		setBoard(createBoard(props.colsCount, props.rowsCount, props.mineCount));
+	}, [props.colsCount, props.rowsCount, props.mineCount]);
+
+	return board.length > 0 ? (
+		<Grid
+			columnCount={props.colsCount}
+			rowCount={props.rowsCount}
+			columnWidth={50}
+			width={500}
+			height={500}
+			rowHeight={50}
+		>
+			{({ columnIndex, rowIndex, style }) => {
+				return createCell(
+					style,
+					twoDimentionalIndexToOne(columnIndex, rowIndex, props.colsCount),
+					board,
+					onMineClick,
+					onNumberTileClick
+				);
+			}}
+		</Grid>
+	) : null;
 }
 
 function createCell(
-  style: object,
-  position: number,
-  board: Tile[],
-  onMineClick: (
-    clickType: ClickType,
-    tileStatus: TileStatus,
-    position: number
-  ) => void,
-  onNumberTileClick: (
-    clickType: ClickType,
-    tileStatus: TileStatus,
-    position: number
-  ) => void
+	style: object,
+	position: number,
+	board: Tile[],
+	onMineClick: (
+		clickType: ClickType,
+		tileStatus: TileStatus,
+		position: number
+	) => void,
+	onNumberTileClick: (
+		clickType: ClickType,
+		tileStatus: TileStatus,
+		position: number
+	) => void
 ) {
-  const boardItem = board[position];
-  if (!boardItem) return null;
+	const boardItem = board[position];
+	if (!boardItem) return null;
 
-  const isMine = boardItem.value === DEFAULT_MINE_TILE.value;
-  let valueProp: any = {};
-  if (!isMine) {
-    valueProp.value = boardItem.value;
-  }
-  return (
-    <TileContainer
-      onClick={isMine ? onMineClick : onNumberTileClick}
-      type={isMine ? 'Mine' : 'NumberTileType'}
-      position={position}
-      key={position}
-      {...valueProp}
-      style={style}
-      status={boardItem.status}
-    />
-  );
+	const isMine = boardItem.value === DEFAULT_MINE_TILE.value;
+	let valueProp: any = {};
+	if (!isMine) {
+		valueProp.value = boardItem.value;
+	}
+	return (
+		<TileContainer
+			onClick={isMine ? onMineClick : onNumberTileClick}
+			type={isMine ? 'Mine' : 'NumberTileType'}
+			position={position}
+			key={position}
+			{...valueProp}
+			style={style}
+			status={boardItem.status}
+		/>
+	);
 }
 
 function revealEmptyTiles(
-  board: Tile[],
-  position: number,
-  colsCount: number,
-  rowsCount: number
+	board: Tile[],
+	position: number,
+	colsCount: number,
+	rowsCount: number
 ): void {
-  let arr: number[] = [position];
+	let arr: number[] = [position];
 
-  while (arr.length > 0) {
-    let currentPosition: number = arr.pop() || 0;
-    const neighbours: number[] = findNeighbours(
-      currentPosition,
-      colsCount,
-      rowsCount
-    );
+	while (arr.length > 0) {
+		let currentPosition: number = arr.pop() || 0;
+		const neighbours: number[] = findNeighbours(
+			currentPosition,
+			colsCount,
+			rowsCount
+		);
 
-    arr.push(
-      ...neighbours
-        .filter((neighbour) => board[neighbour].status === TileStatus.Hidden)
-        .map((neighbourIndex: number) => {
-          board[neighbourIndex].status = TileStatus.Revealed;
-          return neighbourIndex;
-        })
-        .filter((neighbourIndex) => board[neighbourIndex].value === 0)
-    );
-  }
+		arr.push(
+			...neighbours
+				.filter((neighbour) => board[neighbour].status === TileStatus.Hidden)
+				.map((neighbourIndex: number) => {
+					board[neighbourIndex].status = TileStatus.Revealed;
+					return neighbourIndex;
+				})
+				.filter((neighbourIndex) => board[neighbourIndex].value === 0)
+		);
+	}
 }
 
-function toggleFlag(currentState: TileStatus): TileStatus {
-  const options = {
-    [TileStatus.Flag]: TileStatus.Hidden,
-    [TileStatus.Hidden]: TileStatus.Flag,
-  } as { [key in TileStatus]: TileStatus };
+function toggleFlag(currentState: TileStatus, flagCount: number, setFlagCount: (count: number) => void): TileStatus {
+	const options = {
+		[TileStatus.Flag]: TileStatus.Hidden,
+		[TileStatus.Hidden]: TileStatus.Flag,
+	} as { [key in TileStatus]: TileStatus };
 
-  return options[currentState] || currentState;
+	if (currentState === TileStatus.Flag && options[currentState] !== currentState) {
+		setFlagCount(flagCount + 1);
+		return options[currentState] || currentState;
+	}
+	if (flagCount > 0) {
+		setFlagCount(flagCount - 1);
+		return options[currentState] || currentState;
+	}
+	return currentState;
 }
 
 function revealTile(currentState: TileStatus): TileStatus {
-  const options = {
-    [TileStatus.Hidden]: TileStatus.Revealed,
-  } as { [key in TileStatus]: TileStatus };
+	const options = {
+		[TileStatus.Hidden]: TileStatus.Revealed,
+	} as { [key in TileStatus]: TileStatus };
 
-  return options[currentState] || currentState;
+	return options[currentState] || currentState;
 }
 
 export default Board;
--- src/pages/game.page.tsx
+++ src/pages/game.page.tsx
@@ -1,6 +1,8 @@
 import Board from '../components/board/board.component';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './game.page.scss';
+
+export const GameContext = React.createContext<{ flagCount: number, setFlagCount: (count: number) => void, isGameRunning: boolean }>({flagCount: 0, setFlagCount: () => {}, isGameRunning: false});
 
 function Game() {
   const [colsCount, setColsCount] = useState(6);
@@ -11,9 +13,11 @@ function Game() {
     rowsCount,
     minesCount,
   });
+  const [isGameRunning, setIsGameRunning] = useState(true);
+  const [flagCount, setFlagCount] = useState(minesCount);
 
   return (
-    <>
+    <GameContext.Provider value={{ flagCount, isGameRunning, setFlagCount }}>
       <input
         value={colsCount}
         type="number"
@@ -30,19 +34,25 @@ function Game() {
         value={minesCount}
         type="number"
         placeholder="Number of mines"
-        onChange={(e) => setMinesCount(+e.target.value)}
+        onChange={
+          (e) => {
+            setMinesCount(+e.target.value);
+            setFlagCount(+e.target.value);
+          }
+        }
       />
       <button
         onClick={() => setBoardProps({ colsCount, rowsCount, minesCount })}
       >
         New Game
       </button>
+      <div><span>Remaining Flags: {flagCount}</span></div>
       <Board
         colsCount={boardProps.colsCount}
         rowsCount={boardProps.rowsCount}
         mineCount={boardProps.minesCount}
       />
-    </>
+    </GameContext.Provider>
   );
 }
 
