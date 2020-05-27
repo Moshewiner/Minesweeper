48f11a0bd0a38c06b3f2a8fcb99a6d6ce1bc0651
moshew <Moshewiner13@gmail.com>
2020-05-27 16:29:30 +0300

    Added new game button

--- src/pages/game.page.tsx
+++ src/pages/game.page.tsx
@@ -5,16 +5,22 @@ import './game.page.scss'
 
 function Game() {
 
-    let [colsCount, setColsCount] = useState(6);
-    let [rowsCount, setRowsCount] = useState(6);
-    let [minesCount, setMinesCount] = useState(3);
+
+
+    let colsCount = 6;
+    let rowsCount = 6;
+    let minesCount = 3;
+    const [boardProps, setBoardProps] = useState({ colsCount, rowsCount, minesCount });
 
     return (
         <>
-            <input type='number' placeholder="Number of columns" onChange={(e) => setColsCount(+e.target.value)} />
-            <input type='number' placeholder="Number of rows" onChange={(e) => setRowsCount(+e.target.value)} />
-            <input type='number' placeholder="Number of mines" onChange={(e) => setMinesCount(+e.target.value)} />
-            <Board colsCount={colsCount} rowsCount={rowsCount} mineCount={minesCount} />
+            <input type='number' placeholder="Number of columns" onChange={(e) => colsCount = +e.target.value} />
+            <input type='number' placeholder="Number of rows" onChange={(e) => rowsCount = +e.target.value} />
+            <input type='number' placeholder="Number of mines" onChange={(e) => minesCount = +e.target.value} />
+            <button onClick={
+                () => setBoardProps({ colsCount, rowsCount, minesCount })
+            }>New Game</button>
+            <Board colsCount={boardProps.colsCount} rowsCount={boardProps.rowsCount} mineCount={boardProps.minesCount} />
         </>);
 }
 
